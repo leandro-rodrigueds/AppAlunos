9f1a79d9ac1403d1a999bdab4b162ee54948d01d
leandro-rodrigueds <leandrorodrigueds@gmail.com>
2023-03-24 14:06:26 -0300

    up

---
+++ lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class app extends StatelessWidget{
+  const app({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Interface'),
+      ),
+    );
+  }
+  
+}
